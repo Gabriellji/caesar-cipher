01893fe76d5c2391063ebfd251ef33e943347c6e
GabriellJi <gabriellji@yahoo.com>
2020-10-01 23:46:13 +0300

    fix: caesar cipher fn

---
+++ caesar-cipher-cli/caesar-cli.js
@@ -0,0 +1,3 @@
+const {caesar} = require('./modules/caesar.js')
+
+console.log(caesar('aa bb AA BB 11334вапуап', 2));
--- caesar-cipher-cli/modules/caesar.js
+++ caesar-cipher-cli/modules/caesar.js
@@ -1,16 +1,13 @@
 const caesar = (text, shift) => {
-    return text.split('').map((el) => {
-        let isUpperCase = true;
-        let char = el.toUpperCase();
-        let letter = char.charCodeAt(0);
-        if (!(letter > 64 && letter < 91) || !(letter > 96 && letter < 123)) {
-            return el;
-        }
-        if (el !== char) {
-            isUpperCase= false;
-        }
-        letter += shift;
-        char = isUpperCase ? String.fromCharCode(letter) : String.fromCharCode(letter).toLowerCase();
-        return char;
+    return text.split('').map(el => {
+        let letter = el.charCodeAt(0);
+        if ((letter >= 65) && (letter <= 90)) {
+            el = String.fromCharCode(((letter -65 + shift) % 26) + 65);
+        } else if ((letter >= 97) && (letter <= 122)) {
+          el = String.fromCharCode(((letter - 97 + shift) % 26) + 97);
+      }
+      return el;
     }).join('');
   };
+
+  module.exports = {caesar};
